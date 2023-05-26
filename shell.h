03fb902aaa2cfd2a557c8a29417fdfda17d81102
concine007 <natnaelabebe177@hotmail.com>
2023-05-26 16:10:36 -0700

    up

--- shell.h
+++ shell.h
@@ -167,7 +167,7 @@ char **list_to_strings(list_t *);
 size_t print_list(const list_t *);
 list_t *node_starts_with(list_t *, char *, char);
 ssize_t get_node_index(list_t *, list_t *);
-int is_chain(info_t *, char *, size_t *);
+/*int is_chain(info_t *, char *, size_t *);*/
 void check_chain(info_t *, char *, size_t *, size_t, size_t);
 int replace_alias(info_t *);
 int replace_vars(info_t *);
